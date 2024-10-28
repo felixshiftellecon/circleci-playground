68fd6151e2126be79e89532fcd32c94d0ee4ea76
ns-op <ns@efficient-engineering.com>
2024-10-28 12:25:13 -0400

    actually fix header

--- .circleci/job_output_logger.sh
+++ .circleci/job_output_logger.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-CIRCLECI_TOKEN=$CIRCLE_TOKEN
 WORKFLOW_ID=$CIRCLE_WORKFLOW_ID
 VCS_TYPE=$(echo $CIRCLE_BUILD_URL | cut -d'/' -f4)
 
@@ -15,7 +14,7 @@ PROJECT=$CIRCLE_PROJECT_REPONAME
 
 echo "Gathering job_numbers for workflow: ${CIRCLE_WORKFLOW_ID}"
 
-JOBS=$(curl -s -H "Circle-Token: $CIRCLECI_TOKEN" https://circleci.com/api/v2/workflow/${WORKFLOW_ID}/job)
+JOBS=$(curl https://circleci.com/api/v2/workflow/${WORKFLOW_ID}/job -H "Circle-Token: ${CIRCLE_TOKEN}")
 JOB_NUMBERS=$(echo $JOBS | jq -r '.items[].job_number')
 
 JSON_OUTPUT="[]"
@@ -25,20 +24,22 @@ echo "Accessing build data for job numbers: ${JOB_NUMBERS}"
 for JOB_NUMBER in $JOB_NUMBERS
 do
 
-  echo "Gathering build outputs for job: ${JOB_NUMBER}"
+  echo "Gathering build logs for job: ${JOB_NUMBER}"
 
-  JOB_OUTPUT=$(curl -s -H "Circle-Token: $CIRCLECI_TOKEN" "https://circleci.com/api/v1.1/project/${VCS_TYPE}/${USERNAME}/${PROJECT}/${JOB_NUMBER}/output")
+  JOB_OUTPUT=$(curl https://circleci.com/api/v1.1/project/${VCS_TYPE}/${USERNAME}/${PROJECT}/${JOB_NUMBER}/output -H "Circle-Token: ${CIRCLE_TOKEN}")
   JSON_OUTPUT=$(echo $JSON_OUTPUT | jq --arg jobOutput "$JOB_OUTPUT" '. + [{"type": "jobUrl", "content": $jobOutput}]')
   OUTPUT_URLS=$(echo $JOB_OUTPUT | jq -r '.[].url')
   STEP_NAMES=$(echo $JOB_OUTPUT | jq -r '.[].name')
 
   for OUTPUT_URL in $OUTPUT_URLS
   do
-    LOGS=$(curl -s -H "Circle-Token: $CIRCLECI_TOKEN" "$OUTPUT_URL")
+    LOGS=$(curl $OUTPUT_URL -H "Circle-Token: ${CIRCLE_TOKEN}")
     JSON_OUTPUT=$(echo $JSON_OUTPUT | jq --arg logs "$LOGS" '. + [{"type": "stepLogs", "content": $logs}]')
     STEP_NAME=$(echo $STEP_NAMES | jq -r '.[]')
-    echo "Processing step: ${STEP_NAME}"
+    echo "Gathering build logs for step: ${STEP_NAME}"
   done
 done
 
-echo $JSON_OUTPUT > build_logs.json
+mkdir build_logs
+
+echo $JSON_OUTPUT > build_logs/workflow_${CIRCLE_WORKFLOW_ID}build_logs.json
